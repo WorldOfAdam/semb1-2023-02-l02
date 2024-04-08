0e7f1e2bdf4e25d00514691ea482228cd0b0053b
WorldOfAdam <adaofidelesjneto@gmail.com]>
2024-04-08 20:28:36 -0300

    exercicio 1

--- stm32f411-blackpill/src/main.c
+++ stm32f411-blackpill/src/main.c
@@ -25,6 +25,8 @@
 
 #define STM32_GPIOC_BASE     0x40020800     /* 0x48000800-0x48000bff: GPIO Port C */
 
+#define STM32_GPIOC_BASE     0x40020000    /* 0x40020000-0x4002 03ff: GPIO Port A*/
+
 /* Register Offsets *********************************************************/
 
 #define STM32_RCC_AHB1ENR_OFFSET  0x0030   /* AHB1 Peripheral Clock enable register */
@@ -34,7 +36,7 @@
 #define STM32_GPIO_PUPDR_OFFSET   0x000c  /* GPIO port pull-up/pull-down register */
 #define STM32_GPIO_ODR_OFFSET     0x0014  /* GPIO port output data register */
 #define STM32_GPIO_BSRR_OFFSET    0x0018  /* GPIO port bit set/reset register */
-
+#define STM32_GPIO_IDR_OFFSET     0x0010  /* GPIO port input data register */
 
 /* Register Addresses *******************************************************/
 
@@ -46,9 +48,17 @@
 #define STM32_GPIOC_ODR          (STM32_GPIOC_BASE+STM32_GPIO_ODR_OFFSET)
 #define STM32_GPIOC_BSRR         (STM32_GPIOC_BASE+STM32_GPIO_BSRR_OFFSET)
 
+#define STM32_GPIOA_MODER        (STM32_GPIOA_BASE+STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOA_OTYPER       (STM32_GPIOA_BASE+STM32_GPIO_OTYPER_OFFSET)
+#define STM32_GPIOA_PUPDR        (STM32_GPIOA_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOA_ODR          (STM32_GPIOA_BASE+STM32_GPIO_ODR_OFFSET)
+#define STM32_GPIOA_IDR          (STM32_GPIOA_BASE+STM32_GPIO_IDR_OFFSET)
+
 /* AHB1 Peripheral Clock enable register */
 
 #define RCC_AHB1ENR_GPIOCEN      (1 << 2)  /* Bit 2:  IO port C clock enable */
+
+#define RCC_AHB1ENR_GPIOAEN      (1 << 0)  /* Bit 0:  IO port A clock enable */
 
 /* GPIO port mode register */
 
@@ -81,10 +91,14 @@
 
 #define GPIO_BSRR_SET(n)           (1 << (n))
 #define GPIO_BSRR_RESET(n)         (1 << ((n) + 16))
-
+#define GPIO_IDR_MASK(n)           (1 << (n))
 /* Configuration ************************************************************/
 
 #define LED_DELAY  100000
+#define HIGH 1
+#define LOW 0 
+#define BUTTON 0
+#define LED 13
 
 /****************************************************************************
  * Private Types
@@ -114,6 +128,7 @@ int main(int argc, char *argv[])
   /* Ponteiros para registradores */
 
   uint32_t *pRCC_AHB1ENR  = (uint32_t *)STM32_RCC_AHB1ENR;
+
   uint32_t *pGPIOC_MODER  = (uint32_t *)STM32_GPIOC_MODER;
   uint32_t *pGPIOC_OTYPER = (uint32_t *)STM32_GPIOC_OTYPER;
   uint32_t *pGPIOC_PUPDR  = (uint32_t *)STM32_GPIOC_PUPDR;
@@ -128,31 +143,64 @@ int main(int argc, char *argv[])
   /* Configura PC13 como saida pull-up off e pull-down off */
 
   reg = *pGPIOC_MODER;
-  reg &= ~GPIO_MODER_MASK(13);
-  reg |= (GPIO_MODER_OUTPUT << GPIO_MODER_SHIFT(13));
+  reg &= ~GPIO_MODER_MASK(LED);
+  reg |= (GPIO_MODER_OUTPUT << GPIO_MODER_SHIFT(LED));
   *pGPIOC_MODER = reg;  
 
   reg = *pGPIOC_OTYPER;
-  reg &= ~GPIO_OT_MASK(13);
-  reg |= (GPIO_OTYPER_PP << GPIO_OT_SHIFT(13));
+  reg &= ~GPIO_OT_MASK(LED);
+  reg |= (GPIO_OTYPER_PP << GPIO_OT_SHIFT(LED));
   *pGPIOC_OTYPER = reg;
 
   reg = *pGPIOC_PUPDR;
-  reg &= ~GPIO_PUPDR_MASK(13);
-  reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(13));
+  reg &= ~GPIO_PUPDR_MASK(LED);
+  reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(LED));
   *pGPIOC_PUPDR = reg;
+
+
+  uint32_t *pGPIOA_MODER  = (uint32_t *)STM32_GPIOA_MODER;
+  uint32_t *pGPIOA_OTYPER = (uint32_t *)STM32_GPIOA_OTYPER;
+  uint32_t *pGPIOA_PUPDR  = (uint32_t *)STM32_GPIOA_PUPDR;
+  uint32_t *pGPIOA_IDR   = (uint32_t *)STM32_GPIOA_IDR;  
+
+   /* Habilita clock GPIOA*/
+
+  reg  = *pRCC_AHB1ENR;
+  reg |= RCC_AHB1ENR_GPIOAEN;
+  *pRCC_AHB1ENR = reg;
+
+/* Configura PA0 como entrada */
+  reg = *pGPIOA_MODER;
+  reg &= ~GPIO_MODER_MASK(BUTTON);
+  reg |= (GPIO_MODER_OUTPUT << GPIO_MODER_SHIFT(BUTTON));
+  *pGPIOA_MODER = reg;  
+  
+/*Configura PA0 sem pull-up e sem pull-down internos */
+  reg = *pGPIOA_PUPDR;
+  reg &= ~GPIO_PUPDR_MASK(BUTTON);
+  reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR_SHIFT(BUTTON));
+  *pGPIOA_PUPDR = reg;
+
 
   while(1)
     {
       /* Liga LED */
 
-      *pGPIOC_BSRR = GPIO_BSRR_RESET(13);
-      for (i = 0; i < LED_DELAY; i++);
+      /**pGPIOC_BSRR = GPIO_BSRR_RESET(13);
+      for (i = 0; i < LED_DELAY; i++); */
 
       /* Desliga LED */
+     /* * *pGPIOC_BSRR = GPIO_BSRR_SET(13);
+      for (i = 0; i < LED_DELAY; i++); */
 
-      *pGPIOC_BSRR = GPIO_BSRR_SET(13);
-      for (i = 0; i < LED_DELAY; i++);
+      /* Verifica se botao foi pressionado */
+
+      if( ((*pGPIOA_IDR) & GPIO_IDR_MASK(BUTTON)) == LOW) {
+        *pGPIOC_BSRR = GPIO_BSRR_RESET(LED);
+
+      } else if ( ((*pGPIOA_IDR) & GPIO_IDR_MASK(BUTTON)) == HIGH){
+        *pGPIOC_BSRR = GPIO_BSRR_SET(LED); 
+      }
     }
 
   /* Nunca deveria chegar aqui */
